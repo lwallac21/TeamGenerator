1aefffdd0ebbbea1abb4e46223ed4c9a6c30207f
Lawrence Wallace <lwallac21@gmail.com>
2020-11-01 18:48:39 -0500

    fixed tests

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -2,18 +2,18 @@ const Engineer = require("../lib/Engineer");
 
 test("Can set GitHUb account via constructor", () => {
   const testValue = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e.github).toBe(testValue);
+  const employee = new Engineer("Foo", 1, "test@test.com", testValue);
+  expect(employee.github).toBe(testValue);
 });
 
 test("getRole() should return \"Engineer\"", () => {
   const testValue = "Engineer";
-  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
-  expect(e.getRole()).toBe(testValue);
+  const employee = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+  expect(employee.getRole()).toBe(testValue);
 });
 
 test("Can get GitHub username via getGithub()", () => {
   const testValue = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e.getGithub()).toBe(testValue);
+  const employee = new Engineer("Foo", 1, "test@test.com", testValue);
+  expect(employee.getGithub()).toBe(testValue);
 });
--- test/Intern.test.js
+++ test/Intern.test.js
@@ -2,18 +2,18 @@ const Intern = require("../lib/Intern");
 
 test("Can set school via constructor", () => {
   const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
-  expect(e.school).toBe(testValue);
+  const employee = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(employee.school).toBe(testValue);
 });
 
 test("getRole() should return \"Intern\"", () => {
   const testValue = "Intern";
-  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
-  expect(e.getRole()).toBe(testValue);
+  const employee = new Intern("Foo", 1, "test@test.com", "UCLA");
+  expect(employee.getRole()).toBe(testValue);
 });
 
 test("Can get school via getSchool()", () => {
   const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
-  expect(e.getSchool()).toBe(testValue);
+  const employee = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(employee.getSchool()).toBe(testValue);
 });
